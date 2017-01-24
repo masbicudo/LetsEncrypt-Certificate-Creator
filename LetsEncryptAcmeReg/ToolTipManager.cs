ed22d9a35e86903b56cedeacd4aef4cd4e9b4e3e
masbicudo <masbicudo@gmail.com>
2017-01-24 15:41:19 -0200

    Allow more than one tooltip per control.

--- LetsEncryptAcmeReg/ToolTipManager.cs
+++ LetsEncryptAcmeReg/ToolTipManager.cs
@@ -5,18 +5,28 @@ namespace LetsEncryptAcmeReg
 {
     public class ToolTipManager : Control
     {
-        private readonly Dictionary<Control, ToolTipForm> dic = new Dictionary<Control, ToolTipForm>();
+        private readonly Dictionary<Control, Dictionary<object, ToolTipForm>> dic = new Dictionary<Control, Dictionary<object, ToolTipForm>>();
 
-        public ToolTipForm ToolTipFor(Control control)
+        public ToolTipForm ToolTipFor(Control control, object key = null)
         {
-            ToolTipForm form;
-            if (!this.dic.TryGetValue(control, out form))
+            lock (dic)
             {
-                form = new ToolTipForm(control);
-                this.dic.Add(control, form);
-            }
+                Dictionary<object, ToolTipForm> dic2;
+                if (!this.dic.TryGetValue(control, out dic2))
+                {
+                    dic2 = new Dictionary<object, ToolTipForm>();
+                    this.dic.Add(control, dic2);
+                }
 
-            return form;
+                ToolTipForm form;
+                if (!dic2.TryGetValue(key ?? control, out form))
+                {
+                    form = new ToolTipForm(control);
+                    dic2.Add(key ?? control, form);
+                }
+
+                return form;
+            }
         }
     }
 }
