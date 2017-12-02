9ed5abc04e8e15aef9388d83bc63f24c35bf8360
Miguel Angelo <masbicudo@gmail.com>
2017-12-01 23:52:14 -0200

    PathUtils.Simplify to remove unneeded . and .. in the string

--- LetsEncryptAcmeReg/PathUtils.cs
+++ LetsEncryptAcmeReg/PathUtils.cs
@@ -2,13 +2,70 @@ using System;
 
 namespace LetsEncryptAcmeReg
 {
-    static class PathUtils
+    public static class PathUtils
     {
         public static string CreateRelativePath(string path, string basePath)
         {
             if (path.StartsWith(basePath))
                 return path.Substring(basePath.Length);
             throw new NotImplementedException();
+        }
+
+        public static string Simplify(string path)
+        {
+            char[] result = new char[path.Length];
+            int[] starts = new int[path.Length + 1];
+            int curStart = 0;
+            int level = 0;
+            var dots = 0;
+            var posWrite = 0;
+            for (int it = 0; it < path.Length; it++)
+            {
+                var ch = path[it];
+                if (ch == '\\' || ch == '/')
+                {
+                    level++;
+                    level -= dots;
+
+                    if (level < 0)
+                        starts[level = 0] = posWrite + 1;
+
+                    if (dots == 0)
+                        starts[level] = posWrite + 1;
+                    else
+                        posWrite = starts[level] - 1;
+
+                    curStart = it + 1;
+                    dots = 0;
+                }
+                else if (ch == '.' && dots == it - curStart)
+                {
+                    dots++;
+
+                    if (dots > 2)
+                        throw new Exception("Invalid path");
+                }
+
+                if (posWrite >= 0)
+                    result[posWrite] = ch;
+                posWrite++;
+            }
+
+            level++;
+            level -= dots;
+
+            if (level < 0)
+                starts[level = 0] = posWrite + 1;
+
+            if (dots == 0)
+                starts[level] = posWrite + 1;
+            else
+                posWrite = starts[level] - 1;
+
+            if (posWrite >= 0)
+                return new string(result, 0, posWrite);
+
+            return "";
         }
     }
 }
---
+++ Tests/LibsTests.cs
@@ -0,0 +1,32 @@
+﻿using LetsEncryptAcmeReg;
+using LetsEncryptAcmeReg.SSG;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Replaceables;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Tests
+{
+    [TestClass]
+    public class LibsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var str1a = PathUtils.Simplify(".");
+            var str1b = PathUtils.Simplify(".\\");
+            var str1c = PathUtils.Simplify(".\\xpto");
+            var str2 = PathUtils.Simplify("..\\");
+            var str3 = PathUtils.Simplify("..");
+            var str4 = PathUtils.Simplify("..\\..");
+            var str5 = PathUtils.Simplify("..\\..\\");
+            var str6 = PathUtils.Simplify("..\\abc\\..\\");
+            var str7 = PathUtils.Simplify("..\\abc\\..");
+            var str8a = PathUtils.Simplify("abc\\.");
+            var str8b = PathUtils.Simplify("abc\\.\\");
+            var str8c = PathUtils.Simplify("abc\\.\\.");
+        }
+    }
+}
